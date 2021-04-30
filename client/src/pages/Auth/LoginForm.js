25b86021231100a272f1c1e080d14b78340a99db
Jon Free <freejonm@gmail.com>
2021-04-30 14:07:49 -0400

    adding login form from material-ui

--- client/src/pages/Auth/LoginForm.js
+++ client/src/pages/Auth/LoginForm.js
@@ -1,64 +1,190 @@
 import React, { useState } from 'react';
-import { Redirect, Link } from 'react-router-dom';
-// import { Container, Row, Col } from '../../components/Grid';
-// import { Card } from '../../components/Card';
-// import { Input, FormBtn } from '../../components/Form';
-import './index.scss';
+import { Redirect } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-function LoginForm({login}) {
-  const [userObject, setUserObject] = useState({
-    username: '',
-    password: ''
-  });
-  const [redirectTo, setRedirectTo] = useState(null);
+import "./LoginForm.scss";
 
-	const handleChange = (event) => {
-		setUserObject({
-      ...userObject,
-			[event.target.name]: event.target.value
-		});
-	};
-
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		login(userObject.username, userObject.password);
-		setRedirectTo('/');
-	};
-
-  if (redirectTo) {
-    return <Redirect to={{ pathname: redirectTo }} />
-  } else {
-    return (
-      <section className="container">
-        <section className="row">
-          <section className="col-md-3"></section>
-          <section className="col-md-6">
-            <section className="card" title="Login to React Reading List">
-              <form style={{marginTop: 10}}>
-                <label htmlFor="username">Username: </label>
-                <input
-                  type="text"
-                  name="username"
-                  value={userObject.username}
-                  onChange={handleChange}
-                />
-                <label htmlFor="password">Password: </label>
-                <input
-                  type="password"
-                  name="password"
-                  value={userObject.password}
-                  onChange={handleChange}
-                />
-                <a href="/signup">Register</a>
-                <button onClick={handleSubmit}>Login</button>
-              </form>
-            </section>
-          </section>
-          <section className="col-md-3"></section>
-        </section>
-      </section>
-    )
-  }
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+function LoginForm({login}) {
+    const classes = useStyles();
+
+    const [userObject, setUserObject] = useState({
+      username: '',
+      password: ''
+    });
+
+    const [redirectTo, setRedirectTo] = useState(null);
+  
+  	const handleChange = (event) => {
+  		setUserObject({
+        ...userObject,
+  			[event.target.name]: event.target.value
+  		});
+  	};
+  
+  	const handleSubmit = (event) => {
+  		event.preventDefault();
+  		login(userObject.username, userObject.password);
+  		setRedirectTo('/');
+  	};
+  
+    if (redirectTo) {
+      return <Redirect to={{ pathname: redirectTo }} />
+    } else {
+      return (
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <div className={classes.paper}>
+          <Avatar id="login-avatar" className={classes.avatar}>
+            {/* <LockOutlinedIcon /> */}
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            value={userObject.username}
+            onChange={handleChange}
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={userObject.password}
+            onChange={handleChange}
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            id="login-button"
+            className={classes.submit}
+            onClick={handleSubmit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+};
+};
+
 export default LoginForm;
+
+
+
+
+
+// 
+//       <section className="container">
+//         <section className="row">
+//           <section className="col-md-3"></section>
+//           <section className="col-md-6">
+//             <section className="card" title="Login to React Reading List">
+//               <form style={{marginTop: 10}}>
+//                 <label htmlFor="username">Username: </label>
+//                 <input
+//                   type="text"
+//                   name="username"
+//                   value={userObject.username}
+//                   onChange={handleChange}
+//                 />
+//                 <label htmlFor="password">Password: </label>
+//                 <input
+//                   type="password"
+//                   name="password"
+//                   value={userObject.password}
+//                   onChange={handleChange}
+//                 />
+//                 <a href="/signup">Register</a>
+//                 <button onClick={handleSubmit}>Login</button>
+//               </form>
+//             </section>
+//           </section>
+//           <section className="col-md-3"></section>
+//         </section>
+//       </section>
+//     )
+//   }
+// }
+
+

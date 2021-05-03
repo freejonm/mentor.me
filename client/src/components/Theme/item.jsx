b425ea4e0485b6723f054f40f03a5fc9258da007
Sarah Marshall <sr70079@gmail.com>
2021-05-03 12:56:06 -0400

    import makeStyles to item.jsx

--- client/src/components/Theme/item.jsx
+++ client/src/components/Theme/item.jsx
@@ -1,4 +1,19 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      margin: theme.spacing(3),
+      width: 345,
+    },
+    media: {
+      height: 140,
+    },
+    title: {
+      color: theme.palette.primary.main
+    }
+  }))
+
 
 const Item = () => {
     return (

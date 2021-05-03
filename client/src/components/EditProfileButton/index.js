8e568b7f09265ea383ab43d4391fb8949a40e79e
Jennifer Ross <jjannross@gmail.com>
2021-05-03 19:59:21 -0400

    second button option

--- client/src/components/EditProfileButton/index.js
+++ client/src/components/EditProfileButton/index.js
@@ -11,13 +11,13 @@ const theme = createMuiTheme({
         
         text: {
           // Some CSS
-          background: 'linear-gradient(45deg, #eda320 30%, #db784d 90%)',
+          background: 'linear-gradient(45deg, #637f7d 30%, #01444c 90%)',
           borderRadius: 3,
           border: 0,
           color: 'white',
           height: 36,
           padding: '0 20px',
-          boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+          boxShadow: '0 3px 5px 2px rgba((1,42,47, .3)',
         },
       },
     },

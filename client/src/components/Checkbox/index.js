e2a19387f3a98f163f1086753971779e8d67e8d0
Sarah Marshall <sr70079@gmail.com>
2021-05-03 10:33:47 -0400

    checkbox component created

---
+++ client/src/components/Checkbox/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+
+const GreenCheckbox = withStyles({
+  root: {
+    color: green[400],
+    '&$checked': {
+      color: green[600],
+    },
+  },
+  checked: {},
+})((props) => <Checkbox color="default" {...props} />);
+
+export default function CheckboxLabels() {
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+    checkedF: true,
+    checkedG: true,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  return (
+    <FormGroup row>
+      
+      <FormControlLabel
+        control={<GreenCheckbox checked={state.checkedG} onChange={handleChange} name="checkedG" />}
+        label="Custom color"
+      />
+      
+    </FormGroup>
+  );
+}

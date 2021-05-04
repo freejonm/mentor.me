b6a0b821e3d9088b8bc171c69861612bed5835f5
cparros <celparros@gmail.com>
2021-05-04 13:41:49 -0400

    mentee fields

--- server/models/mentee.js
+++ server/models/mentee.js
@@ -9,26 +9,13 @@ let ObjectId = Schema.ObjectId
 // Define userSchema
 const menteeSchema = new Schema({
 
-	personId: { type: ObjectId },
-  profilePicture: { data: Buffer, contentType: String },
-	firstName: { type: String, unique: false },
-	lastName: { type: String, unique: false },
-  pronouns: { type: String, unique: false, required: false },
-  location: { type: String, unique: false, required: false },
-  email: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
-  password: { type: String, unique: false, required: false },
-  meetingsAttended: { type: Number, unique: false, required: false },
-	mentee: { type: Boolean, unique: false, required: true},
-	description: { type: String, unique: false, required: false},
-	desire: { type: String, unique: false, required: false },
-	gradYear: { type: Number, unique: false, required: false },
-	isLookingForMentor: { type: Boolean, unique: false, required: false },
-	education: { type: String, unique: false, required: false },
-	hoursSpentWithMentor: { type: String, unique: false, required: false },
-	lookingForHelpWith: [{ type: String, unique: false, required: false}],
-  currentPosition: { type: String, unique: false, required: false },
-  fieldOfInterest: [{ type: String, unique: false, required: true }],
-  
+
+    mentee: {type: Boolean, unique: false, required: true},
+    technologiesInterestedIn: [{ type: String, unique:false, required: false}],
+    gradYear: {type: Number, unique: false, required: false},
+    isLookingForMentor: {type: Boolean, unique: false, required: false},
+    hoursSpentWithMentor: {type: String, unique: false, required: false},
+    meetingsAttended: {type: Number, unique: false, required: false},
   
 
 });

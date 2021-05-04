8df0c89394f160f1e620f6f3128c0fdee6979486
Sarah Marshall <sr70079@gmail.com>
2021-05-04 12:12:10 -0400

    connection card update 1

--- client/src/components/ConnectionsCard/index.js
+++ client/src/components/ConnectionsCard/index.js
@@ -1,63 +1,89 @@
-import React from "react";
-import './index.scss';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+// import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
+import CardActions from '@material-ui/core/CardActions';
+
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import { orange } from '@material-ui/core/colors';
 
-const useStyles = makeStyles({
-    root: {
-      minWidth: 275,
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-  });
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: orange[800],
+  },
+}));
 
-  export default function ConnectionsCard() {
-    const classes = useStyles();
-    const bull = <span className={classes.bullet}>•</span>;
+export default function ConnectionsCard() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
 
   return (
     <Card className={classes.root}>
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          Connections
-        </Typography>
-        <Typography variant="body2" component="p">
-        <table>
-  <tr>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>Email</th>
-  </tr>
-  <tr>
-    <td>Jill</td>
-    <td>Smith</td>
-    <td>jill@gmail.com</td>
-  </tr>
-  </table>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            {/* <MoreVertIcon /> */}
+          </IconButton>
+        }
+        title="Connections"
+        subheader="Mentor List"
+      />
+      {/* <CardMedia
+        className={classes.media}
+        image=""
+        title="contact"
+       
+      /> */}
 
-         
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          send a message, request a meeting, or video chat with your connections here
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Contact</Button>
+      <CardActions disableSpacing>
+ 
+
       </CardActions>
+      
     </Card>
   );
 };
-        
-  
-
 

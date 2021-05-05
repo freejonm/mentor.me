87aaa683024be9e4c72fcffe7764b27de53e7598
Jon Free <freejonm@gmail.com>
2021-05-05 12:37:49 -0400

    importing dependencies to Details.js

--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -1,4 +1,15 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 function Details() {
     return (

86aa5c95328c9e0865b287bbfa85b18d86127fb8
Jon Free <freejonm@gmail.com>
2021-05-05 19:52:03 -0400

    troubleshooting heroku deployment

--- server/server.js
+++ server/server.js
@@ -32,12 +32,13 @@ app.use(passport.session()); // will call the deserializeUser
 
 // If its production environment!
 if (process.env.NODE_ENV === 'production') {
-	const path = require('path');
+	// const path = require('path');
 	// console.log('YOU ARE IN THE PRODUCTION ENV');
-	app.use('/static', express.static(path.join(__dirname, '../client/build/static')));
-	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, '../client/build/'))
-	});
+	app.use(express.static("client/build"))
+	// app.use('/static', express.static(path.join(__dirname, '../client/build/static')));
+	// app.get('/', (req, res) => {
+	// 	res.sendFile(path.join(__dirname, '../client/build/'))
+	// });
 }
 
 // Add routes, both API and view

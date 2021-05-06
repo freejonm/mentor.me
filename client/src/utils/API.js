fb96b69807a567d3d929d3e44ef3108c4b8add1e
ejbrownlf <ejbrownlf@gmail.com>
2021-05-06 15:54:52 -0500

    added fronted function for grabbing more specific user information

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -9,5 +9,14 @@ export default {
   },
   getAllUsers: function() {
     return axios.get('api/users');
+  },
+  getMentors: function() {
+    return axios.get('api/mentor')
+  },
+  getMentees: function() {
+    return axios.get('api/mentee')
+  },
+  getUserByID: function() {
+    return axios.get('/api/users/:id')
   }
 };

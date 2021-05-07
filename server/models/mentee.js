d575a9ded6769ae3b0f6c17537167c16ce3ca152
ejbrownlf <ejbrownlf@gmail.com>
2021-05-07 14:10:22 -0500

    removed unneeded functions

--- server/models/mentee.js
+++ server/models/mentee.js
@@ -4,37 +4,13 @@ const bcrypt = require('bcryptjs');
 const strategy = require('../passport/localStrategy')
 mongoose.promise = Promise;
 
-let ObjectId = Schema.ObjectId
-
 // Define userSchema
 const menteeSchema = new Schema({
-    mentee: {type: Boolean, unique: false, required: true},
     technologiesInterestedIn: [{ type: String, unique:false, required: false}],
-    isLookingForMentor: {type: Boolean, unique: false, required: false},
-    hoursSpentWithMentor: {type: String, unique: false, required: false},
-    meetingsAttended: {type: Number, unique: false, required: false},
+    isLookingForMentor: {type: Boolean, unique: false, required: false, default: true},
+    hoursSpentWithMentor: {type: Number, unique: false, required: false, default: 0},
+    meetingsAttended: {type: Number, unique: false, required: false, default: 0}
 });
-
-// Define schema methods
-menteeSchema.methods = {
-	checkPassword: function(inputPassword) {
-		return bcrypt.compareSync(inputPassword, this.password);
-	},
-	hashPassword: plainTextPassword => {
-		return bcrypt.hashSync(plainTextPassword, 10);
-	}
-};
-
-// Define hooks for pre-saving
-menteeSchema.pre('save', function(next) {
-	if (!this.password) {
-		// console.log('No password provided!');
-		next();
-	} else {
-		this.password = this.hashPassword(this.password);
-		next();
-	}
-})
 
 // Create reference to User & export
 const Mentee = mongoose.model('Mentee', menteeSchema);

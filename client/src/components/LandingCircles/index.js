af4c86ad2c5ee60153c8eae2fe8c3b2e9d3325f5
JaredWeav <jared.weaver2@gmail.com>
2021-05-07 15:28:23 -0400

    added circles, commented out vh on sticky foot

--- client/src/components/LandingCircles/index.js
+++ client/src/components/LandingCircles/index.js
@@ -3,36 +3,84 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
 const Container = styled(motion.div)`
-  display: flex;
-  justify-content: center;
-
   position: relative;
-  height: 100px;
-  width: 100px;
-  background-image: url("https://jaarvisaccelerator.com/wp-content/uploads/2016/01/mentorship.png");
+  height: 200px;
+  width: 200px;
+  background: transparent;
   border-radius: 50%;
-  background-color:white;
+  background-color: white;
+  white-space: normal;
+  overflow-wrap: normal;
 `;
 
-const LandingCircles = ({ content }) => {
+const InnerText = styled.h3`
+  text-align: center;
+  vertical-align: middle;
+  color: black;
+  margin-top: 60px;
+`;
+
+const LandingCircles = () => {
   return (
-    <Container
-      initial={{ opacity: 0, x: -100 }}
-      animate={{
-        scale: [1, 1.4, 1.4, 1, 1],
-        rotate: [0, 0, 270, 270, 0],
-        borderRadius: ['20%', '20%', '50%', '50%'],
-        opacity: 1,
-        x: 0
-      }}
-      transition={{
-        duration: 2,
-        ease: 'easeInOut',
-        times: [0, 0.2, 0.5, 0.8, 1],
-        // loop: Infinity,
-        repeatDelay: 1
-      }}
-    ></Container>
+    <>
+      <Container
+        initial={{ opacity: 0, x: -100 }}
+        animate={{
+          scale: [1, 1.4, 1.4, 1, 1],
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ['20%', '20%', '50%', '50%'],
+          opacity: 1,
+          x: 0
+        }}
+        transition={{
+          duration: 2,
+          ease: 'easeInOut',
+          times: [0, 0.2, 0.5, 0.8, 1],
+          // loop: Infinity,
+          repeatDelay: 1
+        }}
+      >
+        <InnerText>Step 1</InnerText>
+      </Container>
+      <Container
+        initial={{ opacity: 0, x: -100 }}
+        animate={{
+          scale: [1, 1.4, 1.4, 1, 1],
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ['20%', '20%', '50%', '50%'],
+          opacity: 1,
+          x: 0
+        }}
+        transition={{
+          duration: 2,
+          ease: 'easeInOut',
+          times: [0, 0.2, 0.5, 0.8, 1],
+          // loop: Infinity,
+          repeatDelay: 1
+        }}
+      >
+        <InnerText>Step 2</InnerText>
+      </Container>
+      <Container
+        initial={{ opacity: 0, x: -100 }}
+        animate={{
+          scale: [1, 1.4, 1.4, 1, 1],
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ['20%', '20%', '50%', '50%'],
+          opacity: 1,
+          x: 0
+        }}
+        transition={{
+          duration: 2,
+          ease: 'easeInOut',
+          times: [0, 0.2, 0.5, 0.8, 1],
+          // loop: Infinity,
+          repeatDelay: 1
+        }}
+      >
+        <InnerText>Step 3</InnerText>
+      </Container>
+    </>
   );
 };
 
--- client/src/components/StickyFooter/index.js
+++ client/src/components/StickyFooter/index.js
@@ -22,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
-    minHeight: '100vh',
+    // minHeight: '100vh',
   },
   main: {
     marginTop: theme.spacing(8),

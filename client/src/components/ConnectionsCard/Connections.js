32753eac6573fc1c61f8f71fc9347e5e417bcead
Jennifer Ross <jjannross@gmail.com>
2021-05-07 15:27:11 -0400

    h3 added

--- client/src/components/ConnectionsCard/Connections.js
+++ client/src/components/ConnectionsCard/Connections.js
@@ -5,6 +5,7 @@ import "./List.css";
 export const Connections = ({ children }) => {
   return (
     <div className="list-overflow-container">
+        <h3>Connections</h3>
       <ul className="list-group">
         {children}
       </ul>

dfd09d3dfd4d90da81efba2bfc661e3442a8144b
ejbrownlf <ejbrownlf@gmail.com>
2021-05-07 15:37:40 -0500

    created route to handle retrieving a users connections

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -5,5 +5,6 @@ const userController = require("../../controllers/userController");
 
 router.get('/', userController.getAll);
 router.get('/:id', userController.findById);
+router.get('/connections', userController.getConnections);
 
 module.exports = router;

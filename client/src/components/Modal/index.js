c04f6d10c5f5c0d7bffe12c256c855ea5903f608
Jennifer Ross <jjannross@gmail.com>
2021-05-10 16:07:45 -0400

    fixed edit profile button

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -3,6 +3,28 @@ import React, { useState, useEffect, useRef } from 'react';
 import Modal from 'styled-react-modal';
 import EditProfileForm from '../EditProfileForm';
 import styled from 'styled-components';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiButton: {
+      text: {
+        // background: 'linear-gradient(45deg, #db784d 30%, #eda320 90%)',
+        background: '#db784d',
+        borderRadius: 3,
+        font: 'Righteous', 
+        border: 0,
+        color: 'white',
+        height: 36,
+        padding: '0 20px',
+        margin: '10px',
+        boxShadow: '0 3px 5px 2px rgba((219,120,77, .3)',
+      },
+    },
+  },
+});
 
 const StyledModal = Modal.styled`
     display: grid;
@@ -57,18 +79,27 @@ function EditModal() {
 
   return (
     <div>
-      <button onClick={toggleModal}>Edit Profile</button>
+      <ThemeProvider theme={theme}>
+      <Button onClick={toggleModal}>Edit Profile</Button></ThemeProvider>
       <StyledModal
         isOpen={isOpen}
         onBackgroundClick={toggleModal}
         onEscapeKeydown={toggleModal}
       >
-          <ModalHead>
+          <ModalHead >
               Edit Profile
           </ModalHead>
         <EditProfileForm />
         <img src="https://randomuser.me/api/portraits/women/75.jpg" />
+        <ThemeProvider theme={theme}>
+      <Button>
         <EditAvatar>Change Picture</EditAvatar>
+        </Button>
+      </ThemeProvider>
+
+
+
+        
         <button onClick={toggleModal}>Close me</button>
       </StyledModal>
     </div>

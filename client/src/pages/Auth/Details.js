42333982d918fc864c4c56656aab0696c83ffe18
Jon Free <freejonm@gmail.com>
2021-05-10 13:32:42 -0400

    deleting userInfo component after adding those fields to signup component

--- client/src/pages/Auth/Details.js
+++ client/src/pages/Auth/Details.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import UserInfo from "../../components/UserInfo"  
+
 import UserDetails from "../../components/UserDetails"  
 
 function Details() {
     return (
         <div>
-          <UserInfo />
+          
           <UserDetails />
         </div>
 );

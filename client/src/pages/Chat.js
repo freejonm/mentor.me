b0d08b91c2974f9a3028772d030cd2ccd9ba0abe
cparros <celparros@gmail.com>
2021-05-11 17:42:34 -0400

    styled messagee container input and send button. defined states for before and after login. message and message list

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,3 +1,4 @@
+import { CalendarContent } from '@fullcalendar/common'
 import React, {useState, useEffect} from 'react'
 import io from 'socket.io-client'
 
@@ -6,9 +7,14 @@ const CONNECTION_PORT = 'localhost:3001/'
 
 const Chat = () => {
   // console.log('chat')
+  //Before Login
   const [loggedIn, setLoggedIn] = useState(true)
-  const [room, setRoom] = useState('')
+  const [room, setRoom] = useState('Class of 2021')
   const [userName, setUserName] = useState('')
+
+  //After Login
+  const [message, setMessage] = useState('')
+  const [messageList, setMessageList] = useState([])
 
   useEffect(() => {
     socket = io(CONNECTION_PORT)
@@ -22,7 +28,7 @@ const Chat = () => {
  return (
    <div className='chat-application' style={{ borderRadius: '5px', margin: '6%', height: '50rem', display: 'grid', placeItems: 'center'}}>
    {!loggedIn ? (
-    <div className='chat-login' style={{width:'600px', height: '350px', border: '5px solid 0091ff', borderRadius: '10px', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection:'column', margin: '10px'}}>
+    <div className='chat-login' style={{width:'600px', height: '350px', border: '5px solid #0091ff', borderRadius: '10px', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection:'column', margin: '10px'}}>
       <h1 style={{color: 'white', backgroundColor: '#db784d'}}>You are now in the chat application</h1>
         <div className='chat-inputs' style={{margin: '10px', width: '200px', height: '40px', backgroundColor: 'transparent', paddingLeft: '10px'}}>
           <input type='text' placeholder='Username...' onChange={(e) => {setUserName(e.target.value)}}/>
@@ -31,13 +37,13 @@ const Chat = () => {
         </div>
     </div>
     ) : ( 
-      <div className='chat-container'>
-        <div className='messages'>
+      <div className='chat-container' style={{width:'600px', height: '350px', border: '5px solid #0091ff', borderRadius: '10px', display: 'flex', flexDirection:'column', margin: '10px', flex: '90%', width: '100%'}}>
+        <div className='messages' style={{flex: '80%', width: '100%', backgroundColor: 'white'}}>
         </div>
 
-        <div className='message-inputs'>
-          <input type='text' placeholder='Message...' />
-          <button>Send</button>
+        <div className='message-inputs' style={{flex: '20%', width: '100%', display: 'flex', flexDirection: 'row'}}>
+          <input onChange={(e) => setMessage(e.target.value)} type='text' placeholder='Message...' style={{ flex: '80%', height: 'calc(100% -5px)', border: 'none', borderTop: '5px solid #0091ff', backgroundColor: 'lightgray', paddingLeft: '20px', fontSize: '20px'}}/>
+          <button style={{ backgroundColor: '#db784d', color: 'white', flex: '20%', height: '100%', border: 'none', fontSize: '18px'}}>Send</button>
         </div>
       </div>
     )}

0c3fcf186ffbf9a432ff1b3c14ccfb5b82b03c9e
cparros <celparros@gmail.com>
2021-05-11 16:06:10 -0400

    in chat page added sockuse effect for socket connection port and 3 use states for login room and username. Also added connect to room emit

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import io from 'socket.io-client'
 
+let socket
+const CONNECTION_PORT = 'localhost:3001/'
 
 const Chat = () => {
-  console.log('chat')
+  // console.log('chat')
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [room, setRoom] = useState('')
+  const [userName, setUserName] = useState('')
+
+  useEffect(() => {
+    socket = io(CONNECTION_PORT)
+  }, [CONNECTION_PORT])
+
+  const connectToRoom = () => {
+    socket.emit('join_room', room)
+  }
 
  return (
    <div className='chat-application' style={{ borderRadius: '5px', margin: '6%', height: '50rem', display: 'grid', placeItems: 'center'}}>
     <div className='chat-login' style={{width:'600px', height: '350px', border: '5px solid 0091ff', borderRadius: '10px', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection:'column', margin: '10px'}}>
       <h1 style={{color: 'white', backgroundColor: '#db784d'}}>You are now in the chat application</h1>
         <div className='chat-inputs' style={{margin: '10px', width: '200px', height: '40px', backgroundColor: 'transparent', paddingLeft: '10px'}}>
-          <input type='text' placeholder='Username...' />
-          <input type='room' placeholder='room...' />
-          <button>Enter Chat</button>
+          <input type='text' placeholder='Username...' onChange={(e) => {setUserName(e.target.value)}}/>
+          <input type='room' placeholder='room...' onChange={(e) => {setRoom(e.target.value)}}/>
+          <button onClick={connectToRoom} style={{width: '200px', height: '50px', border: 'none', backgroundColor: '#db784d', color: 'white', marginTop: '10%' }}>Enter Chat</button>
         </div>
     </div>
    </div>

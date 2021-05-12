4cc0410a04f12835dcd427854919d4111543dcc4
Jennifer Ross <jjannross@gmail.com>
2021-05-12 16:03:13 -0400

    changed input fields to material UI

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -7,16 +7,16 @@ import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
-// import Input from '@material-ui/core/Input';
-// import FilledInput from '@material-ui/core/FilledInput';
-// import OutlinedInput from '@material-ui/core/OutlinedInput';
-// import InputLabel from '@material-ui/core/InputLabel';
-// import InputAdornment from '@material-ui/core/InputAdornment';
-// import FormHelperText from '@material-ui/core/FormHelperText';
-// import FormControl from '@material-ui/core/FormControl';
-// import TextField from '@material-ui/core/TextField';
-// import Visibility from '@material-ui/icons/Visibility';
-// import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import Input from '@material-ui/core/Input';
+import FilledInput from '@material-ui/core/FilledInput';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 let socket
 const CONNECTION_PORT = 'localhost:3001/'
@@ -116,12 +116,13 @@ const theme = createMuiTheme({
             style={{
               color: 'white', 
               backgroundColor: '#db784d',
-              marginTop: '10px !important'
+              marginTop: '10px !important',
+              marginBottom: '10px',
             }}>
             Chat Application
           </h3>
         <div>
-        {/* <TextField
+        <TextField
           label="Username"
           id="outlined-start-adornment"
           // className={clsx(classes.margin, classes.textField)}
@@ -130,14 +131,26 @@ const theme = createMuiTheme({
           }}
           variant="outlined"
           onChange={(e) => {setUserName(e.target.value)}}
-        /> */}
+        />
           <br></br>
-          <input type='text' placeholder='Username...' id='login' onChange={(e) => {setUserName(e.target.value)}}/>
+          {/* <input type='text' placeholder='Username...' style={{
+            borderRadius: '10px',
+            width: '200px',
+            height: '50px',
+            marginTop: '30px',
+            marginBottom: '20px'
+            }} onChange={(e) => {setUserName(e.target.value)}}/>
           <br></br>
-          <input type='room' placeholder='Room...' id='login' onChange={(e) => {setRoom(e.target.value)}}/>
+          <input type='room' placeholder='Room...' style={{
+            borderRadius: '10px',
+            width: '200px',
+            height: '50px',
+            marginTop: '50px !important',
+            marginBottom: '30px'
+            }} onChange={(e) => {setRoom(e.target.value)}}/> */}
           <br></br>
 
-          {/* <TextField
+          <TextField
           label="room"
           id="outlined-start-adornment"
           // className={clsx(classes.margin, classes.textField)}
@@ -145,20 +158,18 @@ const theme = createMuiTheme({
             startAdornment: <InputAdornment position="start"></InputAdornment>,
           }}
           variant="outlined"
-          onChange={(e) => {setRoom(e.target.value)}}></TextField> */}
+          onChange={(e) => {setRoom(e.target.value)}}></TextField>
 
+         
 
+          
 
-
-
-          <br></br>
-          <ThemeProvider>
+        </div>
+        <ThemeProvider>
           <Button onClick={connectToRoom}>
               Enter Chat
           </Button>
           </ThemeProvider>
-
-        </div>
     </div>
     ) : ( 
       <div className='chat-container' 

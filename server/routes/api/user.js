cc7e1bb145e4f82c7afa0c315523c993b626223e
ejbrownlf <ejbrownlf@gmail.com>
2021-05-12 14:52:13 -0500

    route for put request to update user

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -6,5 +6,6 @@ const userController = require("../../controllers/userController");
 router.get('/', userController.getAll);
 router.get('/:id', userController.findById);
 router.route('/connections/:id').get(userController.getConnections);
+router.put('/:id', userController.updateUser);
 
 module.exports = router;

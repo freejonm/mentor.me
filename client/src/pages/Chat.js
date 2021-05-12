19a8e64ddcdb10f2d926bfc075356434f23bafd9
cparros <celparros@gmail.com>
2021-05-11 23:24:02 -0400

    added send message function and send message emitter also began testing messagae display

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -8,7 +8,7 @@ const CONNECTION_PORT = 'localhost:3001/'
 const Chat = () => {
   // console.log('chat')
   //Before Login
-  const [loggedIn, setLoggedIn] = useState(true)
+  const [loggedIn, setLoggedIn] = useState(false)
   const [room, setRoom] = useState('Class of 2021')
   const [userName, setUserName] = useState('')
 
@@ -25,25 +25,135 @@ const Chat = () => {
     socket.emit('join_room', room)
   }
 
+  const sendMessage = () => {
+    let messageContent = {
+      room: room,
+      content: {
+        author: userName,
+        message: message
+      }
+    }
+
+    socket.emit('send_message', messageContent)
+    setMessageList([...messageList, messageContent.content])
+    setMessage('')
+  }
+
  return (
-   <div className='chat-application' style={{ borderRadius: '5px', margin: '6%', height: '50rem', display: 'grid', placeItems: 'center'}}>
+   <div className='chat-application' 
+    style={{ 
+      borderRadius: '5px', 
+      margin: '6%', 
+      height: '50rem', 
+      display: 'grid', 
+      placeItems: 'center'}}>
    {!loggedIn ? (
-    <div className='chat-login' style={{width:'600px', height: '350px', border: '5px solid #0091ff', borderRadius: '10px', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection:'column', margin: '10px'}}>
-      <h1 style={{color: 'white', backgroundColor: '#db784d'}}>You are now in the chat application</h1>
-        <div className='chat-inputs' style={{margin: '10px', width: '200px', height: '40px', backgroundColor: 'transparent', paddingLeft: '10px'}}>
+    <div className='chat-login' 
+      style={{
+        width:'600px', 
+        height: '350px', 
+        border: '5px solid #0091ff', 
+        borderRadius: '10px', 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        flexDirection:'column', 
+        margin: '10px'
+        }}>
+          <h1 
+            style={{
+              color: 'white', 
+              backgroundColor: '#db784d'
+            }}>
+            You are now in the chat application
+          </h1>
+        <div className='chat-inputs' 
+          style={{
+            margin: '10px', 
+            width: '200px', 
+            height: '40px', 
+            backgroundColor: 'transparent', 
+            paddingLeft: '10px'}}>
+
           <input type='text' placeholder='Username...' onChange={(e) => {setUserName(e.target.value)}}/>
           <input type='room' placeholder='room...' onChange={(e) => {setRoom(e.target.value)}}/>
-          <button onClick={connectToRoom} style={{width: '200px', height: '50px', border: 'none', backgroundColor: '#db784d', color: 'white', marginTop: '10%' }}>Enter Chat</button>
+
+          <button onClick={connectToRoom} 
+            style={{
+              width: '200px', 
+              height: '50px', 
+              border: 'none', 
+              backgroundColor: '#db784d', 
+              color: 'white', 
+              marginTop: '10%' 
+            }}>
+              Enter Chat
+          </button>
+
         </div>
     </div>
     ) : ( 
-      <div className='chat-container' style={{width:'600px', height: '350px', border: '5px solid #0091ff', borderRadius: '10px', display: 'flex', flexDirection:'column', margin: '10px', flex: '90%', width: '100%'}}>
-        <div className='messages' style={{flex: '80%', width: '100%', backgroundColor: 'white'}}>
+      <div className='chat-container' 
+        style={{
+          width:'600px', 
+          height: '350px', 
+          border: '5px solid #0091ff', 
+          borderRadius: '10px', 
+          display: 'flex', 
+          flexDirection:'column', 
+          margin: '10px', 
+          flex: '90%', 
+          width: '100%'
+        }}>
+
+        <div className='messages' 
+         style={{
+           flex: '80%', 
+           width: '100%', 
+           backgroundColor: 'white'
+          }}>
+            {
+              console.log(messageList),
+              messageList.map((val, key) => {
+              return ( 
+                <h1 style={{color: 'black'}}>
+                  {' '} {val.author} {val.message}</h1> 
+              )
+            })}
         </div>
 
-        <div className='message-inputs' style={{flex: '20%', width: '100%', display: 'flex', flexDirection: 'row'}}>
-          <input onChange={(e) => setMessage(e.target.value)} type='text' placeholder='Message...' style={{ flex: '80%', height: 'calc(100% -5px)', border: 'none', borderTop: '5px solid #0091ff', backgroundColor: 'lightgray', paddingLeft: '20px', fontSize: '20px'}}/>
-          <button style={{ backgroundColor: '#db784d', color: 'white', flex: '20%', height: '100%', border: 'none', fontSize: '18px'}}>Send</button>
+        <div className='message-inputs' 
+          style={{
+            flex: '20%', 
+            width: '100%', 
+            display: 'flex', 
+            flexDirection: 'row'
+          }}>
+
+          <input onChange={(e) => setMessage(e.target.value)} type='text' placeholder='Message...' 
+            style={{ 
+              flex: '80%', 
+              height: 'calc(100% -5px)', 
+              border: 'none', 
+              borderTop: '5px solid #0091ff', 
+              backgroundColor: 'lightgray', 
+              paddingLeft: '20px', 
+              fontSize: '20px'
+            }}/>
+
+          <button 
+            onClick={sendMessage}
+            style={{ 
+              backgroundColor: '#db784d', 
+              color: 'white', 
+              flex: '20%', 
+              height: '100%', 
+              border: 'none', 
+              fontSize: '18px'
+            }}>
+            Send
+          </button>
+          
         </div>
       </div>
     )}

9e4a283127e25af11a66f68e9d6a95d5ad874550
Jennifer Ross <jjannross@gmail.com>
2021-05-13 14:30:00 -0400

    created member profile component called MemberDetails

--- client/src/pages/MemberProfile.js
+++ client/src/pages/MemberProfile.js
@@ -1,5 +1,28 @@
 import React, { useState, useEffect} from 'react';
 import API from '../utils/API';
+import { Link } from 'react-router-dom';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import styled from 'styled-components';
+import MemberDetails from '../components/MemberDetails';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginTop: theme.spacing(15)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    backgroundColor: theme.palette.secondary
+  }
+}));
+
+
+
+
 
 
 function MemberProfile({ match }) {
@@ -12,33 +35,59 @@ function MemberProfile({ match }) {
     API.getUserByID(userId).then(res => {
       console.log(res.data)
       setUserData(res.data);
+      console.log(userData);
     })
     
   }, [userId])
 
-return (
-    <>
-      {/* ignore this line this is just a test to display the user info as a p tag */}
-      <p>{JSON.stringify(userData)}</p>
+  const classes = useStyles();
 
-      {/* firstName: { type: String, unique: false },
-  lastName: { type: String, unique: false },
-  pronouns: { type: String, unique: false, required: false },
-  username: { type: String, unique: false, required: false },
-  password: { type: String, unique: false, required: false },
-  email: {
-    type: String,
-    lowercase: true,
-    required: [true, "can't be blank"],
-    match: [/\S+@\S+\.\S+/, "is invalid"],
-    index: true,
-  },
-  currentPosition: { type: String, unique: false, required: false },
-  yearsExperience: {type: String, unique: false, required: false },
-  education: { type: String, unique: false, required: false },
-  location: */}
-    </>
+return (
+  <>
+      {/* ignore this line this is just a test to display the user info as a p tag */}
+      {/* <p>{JSON.stringify(userData)}</p> */}
+      <div className={classes.root}>
+      <Grid container
+  // direction="row"
+  // justify="center"
+  // alignItems="center"
+>
+        <Grid item xs={9}>
+          <Paper className={classes.paper}>
+          <MemberDetails 
+          userName={userData.username}
+          email={userData.email} 
+          location={userData.location} 
+          currentPosition={userData.currentPosition} yearsExp={userData.yearsExperience} />
+          </Paper>
+
+            
+
+        </Grid>
+        </Grid>
+        {/* <Grid item xs={6}>
+          <Paper className={classes.paper}>
+            {users.length ? (
+              <Connections>
+                {users.map((users) => (
+                  <ConnectionsItem key={users._id}>
+                    <Link to={'/memberprofile/' + users._id}>
+                      <img src={users.profilePicture} />
+                      <ConnectionsName>
+                        {users.firstName} {users.lastName}
+                      </ConnectionsName>
+                    </Link>
+                  </ConnectionsItem>
+                ))}
+              </Connections>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+          </Paper>
+        </Grid> */}
+    </div>
+ </>   
 )
-}
+};
 
 export default MemberProfile;

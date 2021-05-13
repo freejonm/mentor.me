f5f0711d9f7f910b7bdb609e76781fd084e62634
Sarah Marshall <sr70079@gmail.com>
2021-05-12 20:51:04 -0400

    import scss to landing steps component

--- client/src/components/LandingSteps/index.js
+++ client/src/components/LandingSteps/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import "./index.scss";
 
 const useStyles = makeStyles((theme) => ({
   root: {

540055e2665a0dc05b5bfd7364b76955fbb8ba37
Sarah Marshall <sr70079@gmail.com>
2021-05-12 20:42:52 -0400

    landing step grid fix

--- client/src/components/LandingSteps/index.js
+++ client/src/components/LandingSteps/index.js
@@ -20,27 +20,23 @@ export default function CenteredGrid() {
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
+       
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+              <h2>Find Mentors</h2>
+          </Paper>
         </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+              <h2>Eat Salad</h2>
+          </Paper>
         </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+              <h2>Profit</h2>
+          </Paper>
         </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
+        
       </Grid>
     </div>
   );

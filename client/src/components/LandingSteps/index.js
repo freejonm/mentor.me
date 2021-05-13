b108960f77f9b5a5eb1c0a16570d753283c4547b
Sarah Marshall <sr70079@gmail.com>
2021-05-12 20:47:21 -0400

    paper removed from grid

--- client/src/components/LandingSteps/index.js
+++ client/src/components/LandingSteps/index.js
@@ -22,19 +22,19 @@ export default function CenteredGrid() {
       <Grid container spacing={3}>
        
         <Grid item xs={4}>
-          <Paper className={classes.paper}>
+
               <h2>Find Mentors</h2>
-          </Paper>
+
         </Grid>
         <Grid item xs={4}>
-          <Paper className={classes.paper}>
+
               <h2>Eat Salad</h2>
-          </Paper>
+
         </Grid>
         <Grid item xs={4}>
-          <Paper className={classes.paper}>
+
               <h2>Profit</h2>
-          </Paper>
+
         </Grid>
         
       </Grid>

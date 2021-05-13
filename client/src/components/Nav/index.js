da909c26a084abddea53d84f4b58ccee3072f3b0
JaredWeav <jared.weaver2@gmail.com>
2021-05-12 22:11:34 -0400

    navbar update

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Auth from '../../utils/AUTH'
+import Auth from '../../utils/AUTH';
 
 const NavContainer = styled.nav`
   display: inline-flex;
@@ -34,12 +34,35 @@ const NavLinkGroup = styled.ul`
 `;
 
 const NavLink = styled.a`
+  font-family: 'Righteous', sans-serif;
   display: flex;
   justify-content: space-between;
   color: white;
-  font-size: 32px;
+  font-size: 30px;
   margin-right: 15px;
-  font-weight: bold;
+  padding: 8px;
+  border-radius:5px;
+  /* border-left: 5px solid #01444c; */
+  border: 5px solid #01444c;
+  background-color:#eda320;
+
+  &:hover {
+    color: #012a2f;
+    text-decoration: none;
+    cursor: pointer;
+    background-color:#637f7d;
+    /* border-right:none; */
+  /* border-left:none; */
+  }
+`;
+
+
+
+const NavP = styled.a`
+  color: white;
+  font-size: 30px;
+  margin-right: 15px;
+
   &:hover {
     color: #637f7d;
     text-decoration: none;
@@ -47,43 +70,38 @@ const NavLink = styled.a`
   }
 `;
 
-const NavP = styled.a`
-color: white;
-  font-size: 32px;
-  margin-right: 15px;
-  font-weight: bold;
-
-&:hover {
-    color: #637f7d;
-    text-decoration: none;
-    cursor: pointer;
-}
-`;
-
-const Nav = ({loggedIn}) => {
-   const handleSubmit = () =>{
-     Auth.logout().then(res => {
-       window.location.href='/';
-       console.log(res)
-     });
-
-   };
+const Nav = ({ loggedIn }) => {
+  const handleSubmit = () => {
+    Auth.logout().then((res) => {
+      window.location.href = '/';
+      console.log(res);
+    });
+  };
   return (
     <div>
       <NavContainer>
-
         <NavBrand href="/">mentor me</NavBrand>
         <NavLinkGroup>
-          {loggedIn ? <>
-            <NavLink href="/dashboard"> Dashboard </NavLink>
-            <NavLink href="/chat"> Chat </NavLink>
-            <NavLink href="/" onClick={handleSubmit}> <NavP>Logout</NavP></NavLink>
-          </> : (<>
-            <NavLink href="/register"> Getting Started </NavLink>
-          <NavLink href="/login"> Login </NavLink>
-          
-          </>)}
-                    
+          {loggedIn ? (
+            <>
+              <NavLink href="/dashboard"> Dashboard </NavLink>
+              {/* <NavPipe /> */}
+              <NavLink href="/chat"> Chat </NavLink>
+              {/* <NavPipe /> */}
+              <NavLink href="/resourcefeed"> Feed </NavLink>
+              {/* <NavPipe /> */}
+              <NavLink href="/" onClick={handleSubmit}>
+                {' '}
+                <NavP>Logout</NavP>
+              </NavLink>
+            </>
+          ) : (
+            <>
+              <NavLink href="/register"> Getting Started </NavLink>
+              {/* <NavPipe /> */}
+              <NavLink href="/login"> Login </NavLink>
+            </>
+          )}
         </NavLinkGroup>
       </NavContainer>
     </div>
---
+++ client/src/pages/ResourceFeed.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import CreatePost from "../components/CreatePost";
+// import PostList from "../components/PostList";
+
+
+const ResourceFeed = () => {
+    return (
+        <div>
+            <h1>RESOUYRCVE</h1>
+            {/* <CreatePost />
+            <PostList /> */}
+            
+        </div>
+    )
+}
+
+export default ResourceFeed;

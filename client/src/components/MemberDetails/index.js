c029e7369e7306e14642043b37ba3158a16dbeb3
Jennifer Ross <jjannross@gmail.com>
2021-05-13 14:52:59 -0400

    updated container on member format

--- client/src/components/MemberDetails/index.js
+++ client/src/components/MemberDetails/index.js
@@ -7,42 +7,6 @@ import { makeStyles, withStyles } from '@material-ui/core/styles';
 
 import styled from 'styled-components';
 
-const StyledBadge = withStyles((theme) => ({
-  badge: {
-    backgroundColor: '#44b700',
-    color: '#44b700',
-    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-    '&::after': {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      borderRadius: '50%',
-      animation: '$ripple 1.2s infinite ease-in-out',
-      border: '1px solid currentColor',
-      content: '""'
-    }
-  },
-  '@keyframes ripple': {
-    '0%': {
-      transform: 'scale(.8)',
-      opacity: 1
-    },
-    '100%': {
-      transform: 'scale(2.4)',
-      opacity: 0
-    }
-  }
-}))(Badge);
-
-// const SmallAvatar = withStyles((theme) => ({
-//   root: {
-//     width: 600,
-//     height: 600,
-//     border: `2px solid ${theme.palette.background.paper}`
-//   }
-// }))(Avatar);
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -54,33 +18,15 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const UserContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  height: 254px;
-  margin: 0;
-  padding: 0;
-`;
-
-const ColLeft = styled.div`
-  position: relative;
   display: flex;
+  justify-content: center;
   flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-  img {
-    height: 220px;
-    width: 200px;
-    border-radius: 5%;
-    box-shadow: 1px 1px 2px 2px black;
-  }
-`;
+  height: 60vh;
+  width: 50%;
+  align-items: center;
+  margin: auto;
+  padding-top: 400px;
 
-const ColRight = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
-  /* position: relative; */
-  width: auto;
 `;
 
 const UserProfileTitle = styled.h1`
@@ -90,33 +36,7 @@ const UserProfileTitle = styled.h1`
   font-size: 32px;
 `;
 
-const UserInfoContainer = styled.div`
-  display: grid;
-  grid-auto-columns: 1fr;
-  height: auto;
-  width: 100%;
-  margin: 0px;
-  padding: 0px;
-`;
-
-const ListGroup = styled.ul`
-  display: flex;
-
-  flex-direction: column;
-  justify-content: flex-start;
-  /* justify-content:space-evenly; */
-  list-style-type: none;
-  width: 100%;
-
-  li {
-    border: 2px solid #01444c;
-    font-size: 16px;
-    padding: 10px;
-    border-left: 0;
-    border-top: 0;
-    border-right: 0;
-  }
-`;
+ 
 
 
 
@@ -136,35 +56,234 @@ export default function MemberProfile({
   const classes = useStyles();
 
   return (
+    
     <Card>
       <h3>{userName}'s Profile</h3>
       <br></br>
       <CardContent>
         <UserContainer>
-          <ColLeft>
+          
             <UserProfileTitle>{(fullName, pronouns)}</UserProfileTitle>
             <img src="https://randomuser.me/api/portraits/women/75.jpg" />
             {/* <EditAvatar>Change Picture</EditAvatar> */}
-          </ColLeft>
-
-          <ColRight>
-            <ListGroup>
-              <li>Email</li>
+          
+            
+              {/* <li>Email</li>
               <li>Location</li>
               <li>Current Position</li>
               <li>Years Experience</li>
-            </ListGroup>
-          </ColRight>
-          <ColRight>
-            <ListGroup>
+            
+          
               <li>{email}</li>
               <li>{location}</li>
               <li>{currentPosition}</li>
-              <li>{yearsExp}</li>
-            </ListGroup>
-          </ColRight>
+              <li>{yearsExp}</li> */}
+           
         </UserContainer>
       </CardContent>
     </Card>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import Badge from '@material-ui/core/Badge';
+// // import Avatar from '@material-ui/core/Avatar';
+// import Card from '@material-ui/core/Card';
+// import CardContent from '@material-ui/core/CardContent';
+// import { makeStyles, withStyles } from '@material-ui/core/styles';
+
+// import styled from 'styled-components';
+
+// const StyledBadge = withStyles((theme) => ({
+//   badge: {
+//     backgroundColor: '#44b700',
+//     color: '#44b700',
+//     boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+//     '&::after': {
+//       position: 'absolute',
+//       top: 0,
+//       left: 0,
+//       width: '100%',
+//       height: '100%',
+//       borderRadius: '50%',
+//       animation: '$ripple 1.2s infinite ease-in-out',
+//       border: '1px solid currentColor',
+//       content: '""'
+//     }
+//   },
+//   '@keyframes ripple': {
+//     '0%': {
+//       transform: 'scale(.8)',
+//       opacity: 1
+//     },
+//     '100%': {
+//       transform: 'scale(2.4)',
+//       opacity: 0
+//     }
+//   }
+// }))(Badge);
+
+// // const SmallAvatar = withStyles((theme) => ({
+// //   root: {
+// //     width: 600,
+// //     height: 600,
+// //     border: `2px solid ${theme.palette.background.paper}`
+// //   }
+// // }))(Avatar);
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: 'flex',
+//     '& > *': {
+//       margin: theme.spacing(1)
+//     }
+//   }
+// }));
+
+// const UserContainer = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+//   height: 254px;
+//   margin: 0;
+//   padding: 0;
+// `;
+
+// const ColLeft = styled.div`
+//   position: relative;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: flex-start;
+//   align-items: flex-start;
+//   img {
+//     height: 220px;
+//     width: 200px;
+//     border-radius: 5%;
+//     box-shadow: 1px 1px 2px 2px black;
+//   }
+// `;
+
+// const ColRight = styled.div`
+//   display: flex;
+//   justify-content: flex-start;
+//   align-items: flex-start;
+//   /* position: relative; */
+//   width: auto;
+// `;
+
+// const UserProfileTitle = styled.h1`
+//   display: flex;
+//   justify-content: flex-start;
+//   font-family: 'Righteous', sans-serif;
+//   font-size: 32px;
+// `;
+
+// const UserInfoContainer = styled.div`
+//   display: grid;
+//   grid-auto-columns: 1fr;
+//   height: auto;
+//   width: 100%;
+//   margin: 0px;
+//   padding: 0px;
+// `;
+
+// const ListGroup = styled.ul`
+//   display: flex;
+
+//   flex-direction: column;
+//   justify-content: flex-start;
+//   /* justify-content:space-evenly; */
+//   list-style-type: none;
+//   width: 100%;
+
+//   li {
+//     border: 2px solid #01444c;
+//     font-size: 16px;
+//     padding: 10px;
+//     border-left: 0;
+//     border-top: 0;
+//     border-right: 0;
+//   }
+// `;
+
+
+
+
+
+// export default function MemberProfile({
+//   avatar,
+//   userName,
+//   fullName,
+//   pronouns,
+//   email,
+//   currentPosition,
+//   yearsExp,
+//   education,
+//   location
+// }) {
+//   const classes = useStyles();
+
+//   return (
+//     <Card>
+//       <h3>{userName}'s Profile</h3>
+//       <br></br>
+//       <CardContent>
+//         <UserContainer>
+//           <ColLeft>
+//             <UserProfileTitle>{(fullName, pronouns)}</UserProfileTitle>
+//             <img src="https://randomuser.me/api/portraits/women/75.jpg" />
+//             {/* <EditAvatar>Change Picture</EditAvatar> */}
+//           </ColLeft>
+
+//           <ColRight>
+//             <ListGroup>
+//               <li>Email</li>
+//               <li>Location</li>
+//               <li>Current Position</li>
+//               <li>Years Experience</li>
+//             </ListGroup>
+//           </ColRight>
+//           <ColRight>
+//             <ListGroup>
+//               <li>{email}</li>
+//               <li>{location}</li>
+//               <li>{currentPosition}</li>
+//               <li>{yearsExp}</li>
+//             </ListGroup>
+//           </ColRight>
+//         </UserContainer>
+//       </CardContent>
+//     </Card>
+//   );
+// }

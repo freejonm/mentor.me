ed9e067fc73483dd1445555a611c5ebaa1cf896c
ejbrownlf <ejbrownlf@gmail.com>
2021-05-14 11:28:54 -0500

    started two more functions connections controller

--- server/controllers/connectionController.js
+++ server/controllers/connectionController.js
@@ -3,12 +3,21 @@ const db = require("../models");
 
 // Defining methods for the userController
 module.exports = {
+    
   sendFriendRequest: (req, res) => {
 
   },
-  
+
   approveFriendRequest: (req, res) => {
 
+  },
+
+  sendMentorRequest: (req, res) => {
+
+  },
+
+  approveMentorRequest: (req, res) => {
+
   }
-  
+
 };

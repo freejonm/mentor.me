19cd0985a1579bc77bd8b35a32e06d269495d14c
ejbrownlf <ejbrownlf@gmail.com>
2021-05-14 11:25:43 -0500

    initial controller for connection requests

---
+++ server/controllers/connectionController.js
@@ -0,0 +1,14 @@
+const ObjectId = require("mongoose").Types.ObjectId;
+const db = require("../models");
+
+// Defining methods for the userController
+module.exports = {
+  sendFriendRequest: (req, res) => {
+
+  },
+  
+  approveFriendRequest: (req, res) => {
+
+  }
+  
+};

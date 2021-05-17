0dfd3e348d5f7f71562f664f63eaefcc9effa8c0
Sarah Marshall <sr70079@gmail.com>
2021-05-17 19:37:49 -0400

    material button import

--- client/src/components/MemberDetails/index.js
+++ client/src/components/MemberDetails/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiButton: {
+      text: {
+        // background: 'linear-gradient(45deg, #db784d 30%, #eda320 90%)',
+        background: '#db784d',
+        borderRadius: 3,
+        font: 'Righteous', 
+        border: 0,
+        color: 'white',
+        height: 36,
+        padding: '0 20px',
+        margin: '10px',
+        boxShadow: '0 3px 5px 2px rgba((219,120,77, .3)',
+      },
+    },
+  },
+});
+
 
 const UserContainer = styled.div`
   display: flex;

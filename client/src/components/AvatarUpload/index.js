9ade4af8b58fcb679ede579531268347f9946666
JaredWeav <jared.weaver2@gmail.com>
2021-05-17 19:11:35 -0400

    added image-upload in signup

---
+++ client/src/components/AvatarUpload/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import ImageUploader from "react-images-upload";
+
+const AvatarUpload = props => {
+  const [pictures, setPictures] = useState([]);
+
+  const onDrop = picture => {
+    setPictures([...pictures, picture]);
+  };
+  return (
+    <ImageUploader
+      {...props}
+      withIcon={true}
+      onChange={onDrop}
+      imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+      maxFileSize={5242880}
+      name={props}
+      withPreview={true}
+    />
+  );
+};
+
+export default AvatarUpload

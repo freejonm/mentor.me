a5dc2dd1454261c2776eb2687910c892d2fcd095
Jennifer Ross <jjannross@gmail.com>
2021-05-18 13:34:28 -0400

    member detail format fix

--- client/src/pages/EditProfile.js
+++ client/src/pages/EditProfile.js
@@ -7,23 +7,6 @@ return (
     <>
     <EditProfileForm />
     <FormUpdateButton />
-
-      {/* firstName: { type: String, unique: false },
-  lastName: { type: String, unique: false },
-  pronouns: { type: String, unique: false, required: false },
-  username: { type: String, unique: false, required: false },
-  password: { type: String, unique: false, required: false },
-  email: {
-    type: String,
-    lowercase: true,
-    required: [true, "can't be blank"],
-    match: [/\S+@\S+\.\S+/, "is invalid"],
-    index: true,
-  },
-  currentPosition: { type: String, unique: false, required: false },
-  yearsExperience: {type: String, unique: false, required: false },
-  education: { type: String, unique: false, required: false },
-  location: */}
     </>
 )
 }
